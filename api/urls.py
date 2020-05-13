b8830b50d9767c39abb1937454d1a2a1893c581a
KucJloTa <vit96-96@mail.ru>
2020-05-13 21:34:41 +0300

    1

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,21 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
+from .views import PostViewSet, CommentViewSet, FollowViewSet, GroupViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
+
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='posts')
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comment')
+router.register('follow', FollowViewSet, basename='follow')
+router.register('group', GroupViewSet, basename='group')
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,1 +1,51 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.exceptions import PermissionDenied, ParseError
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import viewsets, status, filters
+from .models import Post, Comment, Group, Follow
+from .serializers import PostSerializer, CommentSerializer, GroupSerializer, FollowSerializer
+from .permission import IsAuthorOrReadOnly
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['group', ]
+
+    def perform_create(self, serializer):
+        return serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        return Comment.objects.filter(post=post)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['=user__username', '=following__username', ]
+    permission_classes = [IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [IsAuthorOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['=user__username', '=following__username', ]
